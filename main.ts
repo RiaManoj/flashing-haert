3f3e5a493e06723c14622498489c03ca2081fdb7
RiaManoj <114739801+RiaManoj@users.noreply.github.com>
2022-09-30 11:41:46 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,80 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . #
+        . # . # .
+        . # . . .
+        . . # . #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # . #
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . . # .
+        . . . . #
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . # . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . . .
+        # . # . .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . # . .
+        . # . # .
+        . . . # .
+        # . # . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+})
